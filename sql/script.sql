c574cb868d7d2121af98ca23948ef1c78d989ce1
TERMOUSSI-LAMIAA <lamiaa3105@gmail.com>
2024-09-08 13:11:50 +0100

    createDB

---
+++ sql/script.sql
@@ -0,0 +1,63 @@
+-- Database: biblio
+
+-- DROP DATABASE IF EXISTS biblio;
+
+CREATE DATABASE biblio
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'French_France.1252'
+    LC_CTYPE = 'French_France.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+
+CREATE TABLE bibliotheque (
+    id SERIAL PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL
+);
+CREATE TABLE utilisateur (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50)  NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    type VARCHAR(50) NOT NULL   
+);
+
+CREATE TABLE etudiant (
+    numero VARCHAR(50)
+) INHERITS (utilisateur);
+
+CREATE TABLE professeur (
+    departement VARCHAR(255)    
+) INHERITS (utilisateur);
+
+CREATE TABLE document (
+    id SERIAL PRIMARY KEY,
+titre VARCHAR(255) NOT NULL, 
+    auteur VARCHAR(255) NOT NULL, 
+    datePub DATE NOT NULL, 
+   nbrPages INT,           
+     type VARCHAR(50) NOT NULL,
+    bibliotheque_id INT REFERENCES bibliotheque(id),
+  borrower_id INT REFERENCES utilisateur(id),
+      reserver_id INT REFERENCES utilisateur(id)
+);
+CREATE TABLE livre (
+    isbn VARCHAR(255)         
+) INHERITS (document);
+
+CREATE TABLE magazine (
+    numero VARCHAR(255)       
+) INHERITS (document);
+
+CREATE TABLE journal_scientifique (
+    domaine_recherche VARCHAR(255)
+) INHERITS (document);
+
+CREATE TABLE these_universitaire (
+    universite VARCHAR(255),        
+    domaine VARCHAR(255)
+) INHERITS (document);
+
