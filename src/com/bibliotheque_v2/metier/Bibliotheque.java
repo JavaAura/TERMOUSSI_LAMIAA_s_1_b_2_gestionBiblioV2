6c14048c3ec17a76ac500ce53b00b8897e785f30
TERMOUSSI-LAMIAA <lamiaa3105@gmail.com>
2024-09-14 21:20:04 +0100

    biblio class

---
+++ src/com/bibliotheque_v2/metier/Bibliotheque.java
@@ -0,0 +1,20 @@
+package com.bibliotheque_v2.metier;
+
+public class Bibliotheque {
+
+	  	private int id;
+	    private String nom;
+	    
+	    public Bibliotheque(int id, String nom) {
+	        this.id = id;
+	        this.nom = nom;
+	    }
+
+	    public int getId() {
+	        return id;
+	    }
+	    
+	    public String getNom() {
+	        return nom;
+	    }
+}
